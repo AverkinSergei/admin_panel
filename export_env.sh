afbbdab75384145b0fa99ebe88fef04ffbfe0244
Sergei Averkin <averkin22@gmail.com>
2022-10-19 18:17:23 +0500

    fixes

---
+++ export_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export DB_NAME=movies_database
+export DB_USER=app
+export DB_PASSWORD=123qwe
+export DB_HOST=127.0.0.1
+export DB_PORT=5432
---
+++ run_db_container.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+docker run -d \
+  --name postgres \
+  -p 5432:5432 \
+  -v $HOME/postgresql/data:/var/lib/postgresql/data \
+  -e POSTGRES_PASSWORD=123qwe \
+  -e POSTGRES_USER=app \
+  -e POSTGRES_DB=movies_database  \
+  postgres:13
